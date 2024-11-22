91b089524393311405758d03130bfd3f42c2993b
zzyi <uniminio@qq.com>
2024-11-22 11:10:55 +0800

    ✨ feat: display current time

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import { ref } from "vue";
+import moment from "moment";
+
+const year = ref(0);
+const month = ref(0);
+const day = ref(0);
+const hour = ref(0);
+const min = ref(0);
+const sec = ref(0);
+
+function updateClock() {
+  const time = new Date();
+
+  year.value = time.getFullYear();
+  month.value = time.getMonth();
+  day.value = time.getDay();
+  hour.value = time.getHours();
+  min.value = time.getMinutes();
+  sec.value = time.getSeconds();
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="h-full flex flex-col justify-center">
+    <h1 class="text-center">
+      {{ moment({ hour: hour, minute: min, second: sec }).format("HH:mm:ss") }}
+    </h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
